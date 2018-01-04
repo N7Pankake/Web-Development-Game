39e5e9a17d39559bd461942533de9f1ad52046c0
Oscar Vallarino <oscarito_vallarino@hotmail.com>
2018-01-04 15:54:33 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
+
+var background;
+var backgroundController;
+
+var player;
+
+var cursors;
+
+var mainState = {
+    preLoad:function(){
+    
+        game.load.image('background' , "assets/Background.png");
+        game.load.image('player' , "assets/Link.png");
+},
+     create:function(){
+         background = game.add.tileSprite(0,0,800,600, 'Background');
+         backgroundController = 5;
+         
+         player = game.add.sprite(game.world.centerX,game.world.centerY + 200, 'player');
+         game.physics.enable(player,Phaser.Physics.ARCADE);
+    
+         cursors = game.input.keyboard.createCursorKeys();
+     },
+     update:function(){
+         
+         player.body.velocity.x = 0;
+         
+         background.titlePosition.y += backgroundController;
+         
+         if(cursors.left.isDown){
+             player.body.velocity.x = -350
+         }
+         
+         if(cursors.right.isDown){
+             player.body velocity.x = 350
+         }
+}
+
+}
+
+game.state.add('mainState', mainState);
+game.state.start('mainState');
