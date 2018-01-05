c02f70b026c080060a67f295001f2ca3fdeb81b8
Oscar Vallarino <oscarito_vallarino@hotmail.com>
2018-01-05 02:18:31 +0000

    Map
    is now working, had to change a Tiled option instead of using 'Export tile set as' had to use 'Embed Tileset'

--- scene1.js
+++ scene1.js
@@ -2,7 +2,7 @@ scenes.scene1 = function(){};
 scenes.scene1.prototype = {
     preload: function (){
         game.load.tilemap('level_01', 'Assets/Sprites/Levels/level_01.json' , null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('tiles', 'Assets/Sprites/Levels/zelda_01.png');
+        game.load.image('zelda_01', 'Assets/Sprites/Levels/zelda_01.png');
         
     },
     create: function (){
@@ -10,7 +10,7 @@ scenes.scene1.prototype = {
         addChangeStateEventListeners();
         
         var map = game.add.tilemap('level_01');
-        map.addTilesetImage('zelda_01', 'tiles');
+        map.addTilesetImage('zelda_01');
         
         var level1 = map.createLayer('groundlayer');
     },
